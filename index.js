331bc015c5e0aa80638732ff1af7e934744555a4
Sophie Gu <sophieqgu@gmail.com>
2021-01-17 06:14:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ const k_muppet = {
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
   const { , Bessie, Dolly, Babe, Little} = farmAnimals;
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-  const { , , black_and_white, black, pink } = farmAnimals;
+  const { , black_and_white, black, pink,  } = farmAnimals;
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-  
+
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
